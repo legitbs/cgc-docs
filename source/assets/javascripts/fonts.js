020c31852fdbab7dc020412e8331a38c0a4b78af
Vito Genovese <vito@legitbs.net>
2016-05-18 18:38:56 -0400

    more styling

---
+++ source/assets/javascripts/fonts.js
@@ -0,0 +1,11 @@
+  WebFontConfig = {
+    google: { families: [ 'Open+Sans+Condensed:300,700:latin', 'Open+Sans:400,700,400italic,700italic:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
